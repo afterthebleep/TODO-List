49c1b0786447ed92647b9d1b9547edaad7764079
aishao <afterthebleep@gmail.com>
2017-07-13 16:01:46 -0700

    Refactor and added toggle to input feature

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,14 +2,14 @@
 $("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
-//click on X to delete todo
+//hover todo to delete
 $("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   event.stopPropagation(); // stops bubbling up to other elements
 })
-//
+
 $("input[type='text']").keypress(function(event){
   if(event.which === 13) {
     //get new todo from input
@@ -20,4 +20,8 @@ $("input[type='text']").keypress(function(event){
     $("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span> " + todoText + "</li>")
 
   }
+})
+
+$(".fa-pencil").click(function() {
+  $("input[type='text']").fadeToggle();
 })
